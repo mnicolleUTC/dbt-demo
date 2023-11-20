2dc50b69b3d39fe5da38d87ee043478778111b6f
Mathieu Nicolle <mnicolle.pro@gmail.com>
2023-11-20 19:16:03 +0000

    ✨ Add aircraft correction

--- models/aircraft/dim_aircraft.sql
+++ models/aircraft/dim_aircraft.sql
@@ -1,1 +1,9 @@
-select * from raw.public.aircraft
+WITH FLIGHTCOUNT AS (
+    SELECT "Aircraft_Id", COUNT(*) AS FLIGHTCOUNT
+    FROM individual_flights
+    GROUP BY "Aircraft_Id"
+)
+SELECT a.*, fc.FLIGHTCOUNT
+FROM aircraft a
+LEFT JOIN FLIGHTCOUNT fc 
+ON a."Aircraft_Id" = fc."Aircraft_Id"
